8e1468a98566f937dea203417bcd0c142904c2ef
Felipe Coutinho <felipi@live.com>
2022-04-15 22:47:15 -0300

    update

--- js/app-bd.js
+++ js/app-bd.js
@@ -1,18 +1,23 @@
 // Get a reference to the database service
 const database = firebase.database();
-const usersRef = database.ref('/data');
+const dataRef = database.ref('data');
 
 $(function () {
     $('#addBtn').on('click', function (e) {
         $('#form-equipamentos').submit();
         e.preventDefault();
-        const autoId = usersRef.push().key
-        usersRef.child(autoId).set({
+        const autoId = dataRef.push().key
+        dataRef.child(autoId).set({
+        //dataRef.set({            
             modelo: modelo.value,
             statuss: statuss.value,
             setor: setor.value,
             categoria: categoria.value,
             tags1: tags1.value,
+            tags2: tags2.value,
+            tags3: tags3.value,
+            tags4: tags4.value,
+            tags5: tags5.value,
             guia: guia.value,
             datasheet: datasheet.value,
             manual: manual.value,
@@ -21,3 +26,5 @@ $(function () {
         });
     })
 });
+
+
